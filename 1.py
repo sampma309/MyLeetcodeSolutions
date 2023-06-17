1f3851b8636e7886f0b8c34bd17618489b6414ff
Michael Samp <michael.samp94@gmail.com>
2023-06-17 00:02:25 -0500

    added explanation

--- 1.py
+++ 1.py
@@ -6,3 +6,18 @@ class Solution:
                 return [i, ht[target - nums[i]]]
             else:
                 ht[nums[i]] = i
+
+""" 
+Explanation:
+
+- Create a hash table that will be used to hold the values in the array along
+with their corresponding indices.
+- Iterate over the array and add each value and its index into the hash table
+as a key-value pair
+- If the difference between the target value and the current value has already 
+been seen, return its index along with the current index
+
+Time complexity: O(N)
+Space complexity: O(N)
+
+"""
